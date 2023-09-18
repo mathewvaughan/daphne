6c7d38f2927c5c75858dc55285a6d8c67d21f78a
mathewvaughanwaterfull <mathew@waterfull.io>
2023-09-18 09:09:56 +0100

    add notes and first test

---
+++ src/test_rover.py
@@ -0,0 +1,18 @@
+class Rover:
+    pass
+
+
+def test_reporting_telemetry():
+    rover = Rover()
+
+
+def test_turning_left():
+    pass
+
+
+def test_turning_right():
+    pass
+
+
+def test_moving_forward():
+    pass

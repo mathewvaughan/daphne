181d6c1f98b510071d8c1873a9fbf9cd8a761853
mathewvaughanwaterfull <mathew@waterfull.io>
2023-09-18 09:40:54 +0100

    refactor rover setup into fixture

--- src/test_rover.py
+++ src/test_rover.py
@@ -1,4 +1,11 @@
+import pytest
+
 from src.rover import Rover
+
+
+@pytest.fixture(name="rover")
+def rover_fixture():
+    return Rover(position=[0, 0], direction="N")
 
 
 def test_reporting_telemetry():
@@ -7,8 +14,7 @@ def test_reporting_telemetry():
     assert rover.direction == "N"
 
 
-def test_turning_left():
-    rover = Rover(position=[0, 0], direction="N")
+def test_turning_left(rover):
     rover.left()
     assert rover.direction == "W"
     rover.left()
@@ -19,8 +25,7 @@ def test_turning_left():
     assert rover.direction == "N"
 
 
-def test_turning_right():
-    rover = Rover(position=[0, 0], direction="N")
+def test_turning_right(rover):
     rover.right()
     assert rover.direction == "E"
     rover.right()
@@ -31,25 +36,25 @@ def test_turning_right():
     assert rover.direction == "N"
 
 
-def test_moving_forward_north():
-    rover = Rover(position=[0, 0], direction="N")
+def test_moving_forward_north(rover):
     rover.forward()
     assert rover.position == [0, 1]
 
 
-def test_moving_forward_east():
-    rover = Rover(position=[0, 0], direction="E")
+def test_moving_forward_east(rover):
+    rover.right()
     rover.forward()
     assert rover.position == [1, 0]
 
 
-def test_moving_forward_south():
-    rover = Rover(position=[0, 0], direction="S")
+def test_moving_forward_south(rover):
+    rover.right()
+    rover.right()
     rover.forward()
     assert rover.position == [0, -1]
 
 
-def test_moving_forward_west():
-    rover = Rover(position=[0, 0], direction="W")
+def test_moving_forward_west(rover):
+    rover.left()
     rover.forward()
     assert rover.position == [-1, 0]
